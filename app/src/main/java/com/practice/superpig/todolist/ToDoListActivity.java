3bee2284b5850ca70ec74d77dcac00cce100fcca
superpig11 <wdsz11@gmail.com>
2015-05-03 18:43:49 +0800

    todolist

--- app/src/main/java/com/practice/superpig/todolist/ToDoListActivity.java
+++ app/src/main/java/com/practice/superpig/todolist/ToDoListActivity.java
@@ -3,8 +3,15 @@ package com.practice.superpig.todolist;
 import android.app.Activity;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 
 public class ToDoListActivity extends Activity {
@@ -13,27 +20,33 @@ public class ToDoListActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_to_do_list);
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_to_do_list, menu);
-        return true;
-    }
+        ListView myListView = (ListView) findViewById(R.id.myListView);
+        final EditText myEditText = (EditText) findViewById(R.id.myEditText);
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        final ArrayList<String> todoItems = new ArrayList<>();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+        final ArrayAdapter<String> todoAA;
 
-        return super.onOptionsItemSelected(item);
+        todoAA = new ArrayAdapter<>(this,
+                                            android.R.layout.simple_list_item_1,
+                                            todoItems);
+        myListView.setAdapter(todoAA);
+
+        myEditText.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if (event.getAction() == KeyEvent.ACTION_DOWN){
+                    if ((keyCode == KeyEvent.KEYCODE_DPAD_CENTER)
+                            || (keyCode == KeyEvent.KEYCODE_ENTER)){
+                        todoItems.add(0, myEditText.getText().toString());
+                        todoAA.notifyDataSetChanged();
+                        myEditText.setText("");
+                        return true;
+                    }
+                }
+                return false;
+            }
+        });
     }
 }
